dd7fa2e7e4f184b86b9fd446025fe3914da316ff
karnavr <kraval3@uwo.ca>
2025-10-21 14:53:29 -0400

    start creating publication plots

---
+++ plotting scripts/fuPlots.jl
@@ -0,0 +1,13 @@
+
+
+# dispersion relation(s)
+
+# Will show up as one figure in the thesis:
+# branch 
+# profiles 
+# cauchy error vs. N for a variety of amplitudes
+# condition number vs. N for a variety of amplitudes
+
+# profiles with coeffs 
+
+# a collection of different branches for different parameter values
